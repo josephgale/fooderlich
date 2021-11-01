573541402159ed9043005d1f5ba17814e92f745a
joseph <24618839+josephgale@users.noreply.github.com>
2021-11-01 13:35:42 -0500

    Adding new files for scrollable app

--- 03-basic-widgets/projects/starter/lib/author_card.dart
+++ 03-basic-widgets/projects/starter/lib/author_card.dart
@@ -20,6 +20,8 @@ class AuthorCard extends StatefulWidget {
 }
 
 class _AuthorCardState extends State<AuthorCard> {
+  bool _isFavorited = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -53,15 +55,19 @@ class _AuthorCardState extends State<AuthorCard> {
             ],
           ),
           IconButton(
-            // 4
-              icon: const Icon(Icons.favorite_border),
-              iconSize: 30,
-              color: Colors.grey[400],
-              // 5
-              onPressed: () {
-                const snackBar = SnackBar(content: Text('Favorite Pressed'));
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              }),
+            // 1
+            icon: Icon(_isFavorited ? Icons.favorite : Icons.favorite_border),
+            iconSize: 30,
+            // 2
+            color: Colors.red[400],
+            onPressed: () {
+              // 3
+              setState(() {
+                _isFavorited = !_isFavorited;
+              });
+            },
+          )
+
 
         ],
       ),
