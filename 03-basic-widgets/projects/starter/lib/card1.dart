d46d5a9224fbd634778edece6e2cc35bee0c3c7a
joseph <24618839+josephgale@users.noreply.github.com>
2021-11-01 10:14:48 -0500

    Added card 1 text and text formatting

--- 03-basic-widgets/projects/starter/lib/card1.dart
+++ 03-basic-widgets/projects/starter/lib/card1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
 
 class Card1 extends StatelessWidget {
   const Card1({Key? key}) : super(key: key);
@@ -15,8 +17,43 @@ class Card1 extends StatelessWidget {
     return Center(
       // TODO: Card1 Decorate Container
       child: Container(
-        // TODO: Add a stack of text
-        // 1
+        child: Stack(
+          children: [
+            // 8
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            // 9
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline2,
+              ),
+              top: 20,
+            ),
+            // 10
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            // 11
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            )
+          ],
+        ),
+
+
         padding: const EdgeInsets.all(16),
         // 2
         constraints: const BoxConstraints.expand(
