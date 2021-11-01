2216083e7f015e923a5c219296bf829d7ecbce42
joseph <24618839+josephgale@users.noreply.github.com>
2021-11-01 10:59:16 -0500

    Added chips to card3

--- 03-basic-widgets/projects/starter/lib/card3.dart
+++ 03-basic-widgets/projects/starter/lib/card3.dart
@@ -95,6 +95,11 @@ class Card3 extends StatelessWidget {
                         style: FooderlichTheme.darkTextTheme.bodyText1),
                     backgroundColor: Colors.black.withOpacity(0.7),
                   ),
+                  Chip(
+                    label: Text('Germ',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
                 ],
               ),
             ),
