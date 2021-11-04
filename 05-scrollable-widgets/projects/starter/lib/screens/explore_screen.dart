479ccad920ed4872c7504b63fc69e735eb8cad84
joseph <24618839+josephgale@users.noreply.github.com>
2021-11-03 22:13:22 -0500

    Added notes to explore_screen.dart

--- 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
+++ 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
-
 import '../components/components.dart';
 import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
 
+//JG - This is the entire explore screen widget in screens directory
 class ExploreScreen extends StatelessWidget {
-  // 1
+  //JG - mock_fooderlick_service.dart provides the fx for this variable
   final mockService = MockFooderlichService();
-
+  //JG - this is the constructor
   ExploreScreen({Key? key}) : super(key: key);
-
+  //JG - the build fx returns the results of a fake API request
   @override
   Widget build(BuildContext context) {
-    // 1
+    // 1 FutureBuilder fx returns the results of an API request
     return FutureBuilder(
-      // 2
+      // 2 I guess the future key/argument contains the API endpoint
       future: mockService.getExploreData(),
-      // 3
+      // 3 The builder key appears to take an anonymous fx
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
-        // 4
+        // 4 Not sure how snapshots work yet..similar to firebase
         if (snapshot.connectionState == ConnectionState.done) {
-          // 5
+          // 5 If the connnection works, then return the listview
           return ListView(
-            // 6
+            // 6 ListView() takes 2 args or props: scrollDir and children
             scrollDirection: Axis.vertical,
             children: [
-              // 7
+              // 7 this fx is imported from components.dart
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
               // 8
               const SizedBox(height: 16),
